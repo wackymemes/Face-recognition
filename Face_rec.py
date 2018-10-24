bcfe84ce2fe83df151bd75a14c92c3a86d4c62ea
Tommi Honkanen <tommi.honkanen@student.tut.fi>
2018-10-24 08:32:45 +0300

    Base version of the face recognition

---
+++ Face_rec.py
@@ -0,0 +1,179 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 24 06:05:57 2018
+
+@author: Mikko, modified by Tommi Honkanen
+"""
+
+import os
+import sys
+import cv2
+import dlib
+import numpy as np
+import time
+import threading
+import glob
+
+from skimage import io
+
+face_recognition = dlib.face_recognition_model_v1('./models/dlib_face_recognition_resnet_model_v1.dat')
+face_detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor('./models/shape_predictor_68_face_landmarks.dat')
+
+enrolled_faces = {}
+
+# Semaphor for identifying thread
+identifying = False
+
+def face_to_vector(image, face):
+  return (
+    np
+    .array(face_recognition.compute_face_descriptor(image, face))
+    .astype(float)
+  )
+
+def faces_from_image(image):
+  UPSAMPLING_FACTOR = 0
+  faces = [
+    (face.height() * face.width(), shape_predictor(image, face))
+    for face in face_detector(image, UPSAMPLING_FACTOR)
+  ]
+  return [face for _, face in sorted(faces, reverse=True)]
+
+def image_from_file(path):
+  return io.imread(path)
+
+def identify(image):
+  # Get all faces
+  faces = faces_from_image(image)
+  # Pick largest face
+  face = faces[0] if faces else None
+  
+  # Calculate face descriptor
+  descriptor = face_recognition.compute_face_descriptor(image, face)
+  face_vector = np.array(descriptor).astype(float)
+
+  # THIS is probably hazardous as ordering may not be always the same?
+  enroll_identifiers = np.array(list(enrolled_faces.keys()))
+  enroll_matrix = np.array(list(enrolled_faces.values()))
+
+  # Calculate differences between the face and all enrolled faces
+  differences = np.subtract(np.array(enroll_matrix), face_vector)
+  distances = np.linalg.norm(differences, axis=1)
+  # and pick the closest one
+  closest_index = np.argmin(distances)
+
+  return enroll_identifiers[closest_index], distances[closest_index]
+
+
+def handle_frame(origFrame, cb):
+  #global identifying
+  try:
+    frame = cv2.resize(origFrame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+
+    start = time.time()
+    identifier, distance = identify(frame)
+    if (distance < 0.6):
+       cb(identifier, distance, time.time() - start)
+    else:
+       cb('-', distance, time.time() - start)
+    sys.stdout.flush()
+
+  except Exception as e:
+    exc = e
+    cb(None, 0, time.time() - start)
+    # print(e)
+
+  #identifying = False
+
+def webcam(cb):
+  global identifying
+
+  video_capture = cv2.VideoCapture(0)
+
+  while True:
+    video_capture.grab()
+    if (not identifying):
+      ret, frame = video_capture.retrieve()
+
+      if (ret == False):
+        print('No frame')
+        break
+      identifying = True
+
+      thread = threading.Thread(target=handle_frame, args=(frame, cb))
+      thread.daemon=True
+      thread.start()
+
+  # When everything is done, release the capture
+  video_capture.release()
+
+def load_enrolled_faces():
+  global enrolled_faces
+  enrolled_faces = np.load('faces.npy').item()
+
+def enroll_face(image, name):
+  try:
+    # find face
+    faces = faces_from_image(image)
+    # Pick largest face
+    face = faces[0] if faces else None
+    # face to vector
+    face_vector = face_to_vector(image, face)
+    # save to enrolled faces list
+    enrolled_faces[name] = face_vector
+    # save npy file
+  except Exception as e:
+    print('Unable to enroll', name)
+    print(e)
+
+
+def logger(identifier, distance, duration):
+  if (identifier == '-'):
+    print('Unknown person, operation took', duration, 'seconds.')
+  elif (identifier == None):
+    print('No face recognised, operation took',duration, 'seconds.')
+  else:
+    print('The person in question is', identifier, 'with calculated distance of', distance, ',operation took', duration, 'seconds.')
+
+def enrollImages():
+  print('Enrolling all images')
+  for filename in glob.glob('faces/*.jpg'):
+    print(filename)
+    name = filename.split('/')[0].split('.')[0]
+    name = name.split('\\')[1]
+    image = image_from_file(filename)
+    enroll_face(image, name)
+    print('Enrolled', name)
+  np.save('faces.npy', enrolled_faces)
+  print('Saved faces.npy')
+
+
+def loadTestImages():
+  print('Enrolling all test images')
+  for filename in glob.glob('test_faces/*.jpg'):
+    print(filename)
+    name = filename.split('/')[1].split('.')[0]
+    image = image_from_file(filename)
+    enroll_face(image, name)
+    print('Enrolled', name)
+  np.save('test_faces.npy', enrolled_faces)
+  print('Saved test_faces.npy')
+
+#The main script that runs the image enrolling and testing
+#To reset the faces, delete faces.npy from the script's folder
+def main():
+      
+    if (not os.path.isfile('faces.npy')):
+        enrollImages()
+    else:
+        print('Loading enrolled faces from faces.npy')
+        load_enrolled_faces()
+
+    for filename in glob.glob('test_faces/*.jpg'):
+        
+        print('Testing', filename)
+        image = image_from_file(filename)
+        handle_frame(image, logger)
+
+main()
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Face-recognition
 The code for face recognition in the project
+
+This is a local Windows-based version of the face recognition system. Based on Mikko Pohja's laervi project on Github.
